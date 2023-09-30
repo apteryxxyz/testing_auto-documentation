1b9368de87c4244fdc2ce44b909dcdb6acaf5327
Apteryx <apteryx.xyz@gmail.com>
2023-09-30 18:30:17 +1300

    Commit all changes

--- packages/testing_auto-documentation/release.config.js
+++ packages/testing_auto-documentation/release.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   hooks: {
     'before:init': 'pnpm run prepack && git add docs/api.json',
-    'after:bump': 'npx auto-changelog -p',
+    'after:bump': 'npx auto-changelog@latest -p',
   },
   git: {
     requireCleanWorkingDir: false,
     changelog:
-      'npx auto-changelog --stdout --sort-commits date  --commit-limit false -u --template https://raw.githubusercontent.com/release-it/release-it/main/templates/changelog-compact.hbs',
+      'npx auto-changelog@latest --stdout --sort-commits date  --commit-limit false -u --template https://raw.githubusercontent.com/release-it/release-it/main/templates/changelog-compact.hbs',
     commitMessage: 'Release @apteryxxyz/testing_auto-documentation v${version}',
     tagAnnotation: 'Release @apteryxxyz/testing_auto-documentation v${version}',
   },
